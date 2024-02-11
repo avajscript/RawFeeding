3112239f89bd5a58b45a74a454558a8d7175dc49
Matthew Pierce <lesbianhaircut@outlook.com>
2024-02-11 13:31:37 -0500

    First commit

---
+++ src/main/java/com/example/rawfeeding/RawFeedingApplication.java
@@ -0,0 +1,13 @@
+package com.example.rawfeeding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawFeedingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RawFeedingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rawfeeding/RawFeedingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rawfeeding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawFeedingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
